1ca6b27e269d8278d07ed8b62aadf6204d79fcc4
Karolina Okła <karolina@MBP-Karolina.Dlink>
2022-07-12 16:11:25 +0200

    Finish setup and start of a timer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+
+import {ButtonModule} from 'primeng/button';
+
+const PRIME_MODULES = [
+  ButtonModule
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    PRIME_MODULES
   ],
   providers: [],
   bootstrap: [AppComponent]
